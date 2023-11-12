ced000e4a142ce3cc7acd1cb077c850c389ec5b8
jahangeer7704 <jahangeer7704@gmail.com>
2023-11-12 16:10:17 +0530

    kl

--- src/Header.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Header({value}) {
-  return (
-   <h1 className='text-2xl text-center uppercase font-extrabold  mt-4 '>{value}</h1>
-  )
-}
-
-export default Header
